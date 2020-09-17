04573f36959de71d94c9d7849b5a64fba0aec1d6
Aditya Ron Adhikari(mac) <aditya.adhikari@incred.com>
2020-09-18 01:53:57 +0530

    diff button

--- src/main.js
+++ src/main.js
@@ -173,7 +173,6 @@ function createmenu(){
 
 }
 
-console.log()
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/render.js
+++ src/render.js
@@ -1,5 +1,16 @@
 const jsoneditor = require('jsoneditor')
+const jsonDiff = require('json-diff')
+let diff_on = false;
+let json_err = false
 
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('json_error', () => {
+  alert("CANNOT FIND DIFF IF JSON HAS ERROR")
+});
 
 const editor = document.getElementById("editor")
 const editor2 = document.getElementById("editor2")
@@ -7,8 +18,18 @@ const options = {
     modes : ["tree", "code", "form"], 
     mode : "code",
     search : true,
-    onValidationError : (err)=>{
-        console.log(err)
+    onValidationError : function(err){
+        if(err && err.length){
+            this.container.style.outline = "3px solid orangered"
+            json_err = true;
+            if(diff_on) myEmitter.emit('json_error');
+        }else{
+            json_err = false
+            this.container.style.outline = "transparent"
+        }
+    },
+    onModeChange : (mode)=>{
+        console.log(mode)
     }
 }
 const editor_one = new jsoneditor(editor,options)
@@ -16,6 +37,21 @@ const editor_two = new jsoneditor(editor2,options)
 
 let diff = (event)=>{
     event.target.classList.toggle("active")
+    diff_on = !diff_on
+    if(diff_on && json_err){
+        myEmitter.emit('json_error');
+        event.target.classList.toggle("active")
+        diff_on = !diff_on
+    }else if(diff_on && !json_err){
+        document.getElementById("error_number").innerText = "19";
+        document.getElementById("multiple").innerText = "(s)";
+    }else{
+        document.getElementById("error_number").innerText = "";
+        document.getElementById("multiple").innerText = "";
+    }
+    // const one = editor_one.get()
+    // const two = editor_two.get()
+    // console.log(jsonDiff.diff(one,two))
 }
 
 const controls = Array.from( document.getElementsByClassName('control') )
@@ -35,3 +71,6 @@ controls.forEach(ele=>{
 
 
 
+
+
+// https://github.com/josdejong/jsoneditor/issues/603
