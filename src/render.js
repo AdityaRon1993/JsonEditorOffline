29bf100cb5b337fa56c3df6c509f9ec02e509a11
Aditya Ron Adhikari(mac) <aditya.adhikari@incred.com>
2020-09-19 00:45:27 +0530

    first show diff successfull

--- src/render.js
+++ src/render.js
@@ -15,7 +15,7 @@ myEmitter.on('json_error', () => {
   alert("CANNOT FIND DIFF IF JSON HAS ERROR")
 });
 
-const editor = document.getElementById("editor")
+const editor1 = document.getElementById("editor")
 const editor2 = document.getElementById("editor2")
 const options = {
     modes : ["tree", "code", "form"], 
@@ -35,7 +35,7 @@ const options = {
         console.log(mode)
     }
 }
-const editor_one = new jsoneditor(editor,options)
+const editor_one = new jsoneditor(editor1,options)
 const editor_two = new jsoneditor(editor2,options)
 
 let diff = (event)=>{
@@ -46,15 +46,14 @@ let diff = (event)=>{
         event.target.classList.toggle("active")
         diff_on = !diff_on
     }else if(diff_on && !json_err){
-        document.getElementById("error_number").innerText = "19";
-        document.getElementById("multiple").innerText = "(s)";
+        colourEditor(editor_one,jsonDiff.diff(editor_one.get(),editor_two.get()))
+        colourEditor(editor_two,jsonDiff.diff(editor_two.get(),editor_one.get()))
+        // document.getElementById("error_number").innerText = "19";
+        // document.getElementById("multiple").innerText = "(s)";
     }else{
         document.getElementById("error_number").innerText = "";
         document.getElementById("multiple").innerText = "";
     }
-    // const one = editor_one.get()
-    // const two = editor_two.get()
-    // console.log(jsonDiff.diff(one,two))
 }
 
 const controls = Array.from( document.getElementsByClassName('control') )
@@ -81,7 +80,6 @@ controls.forEach(ele=>{
 
 
 const hide_side = function(){
-    console.log(this)
     document.querySelector(".sidemenu").classList.toggle("hide-menu")
     document.querySelector(".side-menu-background").classList.toggle("hide-side-menu-back")
 }
@@ -91,4 +89,62 @@ document.querySelector('.side-menu-background').addEventListener('click',hide_si
 
 
 
+
+function colourEditor(editor, diffmap){
+    console.log(diffmap)
+    if(editor.getMode() !="tree") {alert("FUCK YOU"); return;}
+    editor.expandAll();
+    const map = editor.node.childs
+    colorObject(map,diffmap)
+}
+
+
+function colorChildren(childNode, path,color,isColorAllChild = false){
+    childNode.dom[path].style.backgroundColor = color
+    if(isColorAllChild && childNode.childs && childNode.childs.length){
+        childNode.childs.forEach(ele=>{
+            colorChildren(ele,path,color,true)
+        })
+    }
+}
+
+
+function colorObject(node,diffmap){
+    if(typeof diffmap != "object"){
+        colorChildren(node,'tree',"pink")
+    }
+    for(key in diffmap){
+        currentData = diffmap[key]
+        if(key.indexOf("__deleted") > -1){
+            const realKey = key.replace("__deleted","")
+            node.map(ele=>{
+                if(ele.field == realKey){
+                    colorChildren(ele,"tree","rgba(233,87,63,.3)", true) 
+                    // if(typeof currentData === "object" &&  Object.keys(currentData).length){
+                    // }
+                }
+            })
+        }else{
+            node.map(ele=>{
+                if(ele.field == key){
+                    console.log("normal" ,key)
+                    colorChildren(ele,"field","pink")
+                    if(ele.childs && ele.childs.length){
+                        if(Array.isArray(currentData)){
+                            const workWith = currentData.filter(ele=>ele[0]!="+")
+                            workWith.forEach((arr,i)=>{
+                                if(arr[0]=='-'){
+                                    colorObject(ele.childs[i].childs || ele.childs[i],arr[1])
+                                }
+                            })
+                        }
+                        colorObject(ele.childs,diffmap)
+                    }else{
+                        colorChildren(ele,"value","purple")
+                    }
+                }
+            })
+        }
+    }
+}
 // https://github.com/josdejong/jsoneditor/issues/603
